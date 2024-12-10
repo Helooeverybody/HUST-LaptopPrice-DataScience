d520a7087b92724690b3ce1fc593c2130afa7c43
nzd1001 <corna1hb@gmail.com>
2024-12-10 21:58:44 +0700

    update update

--- UI/pages/update.py
+++ UI/pages/update.py
@@ -1,17 +1,25 @@
 import streamlit as st
 from streamlit import session_state as ss
 import pandas as pd
+import os
+print(os.getcwd())
+from pages.data_updating_tools.update import a_mess,merge
 def update_data():
     d={"apple":[1,2,3],"banana":["a","b","b"]}
     return pd.DataFrame(d)
 def main():
     st.dataframe(ss.lap_data)
     if st.button("Update data"):
+        myhahaha = a_mess("data/")
         try:
-            new_df=update_data()
+            if myhahaha is False:
+                raise Exception()
+            else:
+                merge(myhahaha,"data/")
+                
         except Exception as e:
             st.write("Something failed while updating data")
         else:
-            ss.lap_data=new_df
+            ss.lap_data=myhahaha
             st.rerun()
         
